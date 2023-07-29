117e23fa4ce40ad827c3cdb3674bb4da5562c67f
Victor <borismustanger@gmail.com>
2023-07-29 22:08:01 +0800

    form

--- src/pages/ReviewsPage/index.js
+++ src/pages/ReviewsPage/index.js
@@ -2,7 +2,14 @@ const ReviewsPage = () => {
     return (
         <div className="pt-5">
             <h1>Оставьте отзыв!</h1>
-            <input className="border-2"/>
+            <form className="">
+                <label for="name">Ваше Имя:</label>
+                <input type="text" id="name" placeholder="введите ваше Имя..." /><br />
+                <label for="review">Отзыв:</label>
+                <textarea type="text" id="review" placeholder="" /><br />
+                <input  type="submit"/>
+            </form>
+
         </div>
 
     );
